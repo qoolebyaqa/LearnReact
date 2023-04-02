950085b4aa4b163725ba4d681c6c503597873a07
Ten <Qoolebyaqa@gmail.com>
2023-04-02 21:08:02 +0500

    fix: add the input erasing

--- react-ts/src/components/searchbar.tsx
+++ react-ts/src/components/searchbar.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function SearchBar() {
   const [cur, setter] = useState(localStorage.searched ? localStorage.searched : '');
+  const searcherRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     return () => {
@@ -11,6 +12,8 @@ function SearchBar() {
 
   const deleteFromLocal = function () {
     localStorage.removeItem('searched');
+    (searcherRef.current as HTMLInputElement).value = '';
+    setter('');
   };
 
   const ChangeInput = (e?: React.FormEvent<HTMLInputElement>) => {
@@ -21,7 +24,7 @@ function SearchBar() {
     <div className="search__section">
       <h3>Storage</h3>
       <div className="search__input-wrap">
-        <input className="search__input" value={cur} onChange={ChangeInput} />
+        <input className="search__input" value={cur} onChange={ChangeInput} ref={searcherRef} />
         <button className="search__button" type="submit" onClick={deleteFromLocal}>
           Remove From Storage
         </button>
